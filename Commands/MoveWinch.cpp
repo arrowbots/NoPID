d092f8722117583ce22e575cac91d6f335a9e3fb
Arrowbots <team3705@gmail.com>
2013-02-15 22:19:40 -0500

    Updating Code
    Has code to control the relays, get input from the limit switches.

--- Commands/MoveWinch.cpp
+++ Commands/MoveWinch.cpp
@@ -8,6 +8,8 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in th future.
 #include "MoveWinch.h"
+#include "WPILib.h"
+
 MoveWinch::MoveWinch() {
 	// Use requires() here to declare subsystem dependencies
 	// eg. requires(chassis);
@@ -22,16 +24,21 @@ void MoveWinch::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void MoveWinch::Execute() {
 	Robot::winch->Move(Robot::oi->getStick2());
-	Robot::oi->PrintNumber("winch encoder",Robot::winch->Read());
+	//printf("Hello World");
+	//Robot::winch->Read(); //Added by Ariful to check if it was being read.
+	//Robot::oi->PrintNumber("winch encoder",Robot::winch->Read());
 }
+
 // Make this return true when this Command no longer needs to run execute()
 bool MoveWinch::IsFinished() {
 	return false;
 }
+
 // Called once after isFinished returns true
 void MoveWinch::End() {
 	Robot::winch->Stop();
 }
+
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void MoveWinch::Interrupted() {
