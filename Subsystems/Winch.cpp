0a164408e2fe049b577cc5fdf31670277b5ddb3f
DanYoSon <daniel.idzerda@gmail.com>
2013-03-10 19:21:57 -0300

    Clean old code and add some comments

--- Subsystems/Winch.cpp
+++ Subsystems/Winch.cpp
@@ -19,36 +19,7 @@ void Winch::InitDefaultCommand() {
 	SetDefaultCommand(new MoveWinch());
 }
 void Winch::Move(Joystick* stick) {
-	//printf("Base Limit %d and Encoder %f \n",baseLimit->Get(), encoder->GetDistance());
-	//if the y axis of the stick has a value greater than 0.025 and less than -0.025
-
-	/*if (stick->GetY() > 0.05 && baseLimit->Get() == 1) {
-	 //sets the speed of the motors to 75% of the value of the stick
-	 cim3->Set(stick->GetY()*0.75);
-	 cim4->Set(stick->GetY()*0.75);
-	 } else if (stick->GetY() < -0.05) {
-	 cim3->Set(stick->GetY()*0.75);
-	 cim4->Set(stick->GetY()*0.75);
-	 } else if (stick->GetY() < 0.05 && stick->GetY() > -0.05) {
-	 cim3->Set(0);
-	 cim4->Set(0);
-	 }
-	 */
-	//printf("Distance: %f4 \n",encoder->GetDistance());
-	/*double distance = encoder->GetDistance();
-	if (distance <= (1000) && stick->GetY() <= -0.05) {
-		printf("Greater than -1000\n");
-		printf("Distance: %f4 \n",encoder->GetDistance());
-		cim1->Set(stick->GetY());
-	} else if (distance >= (-1000) && stick->GetY() >= 0.05) {
-		printf("Less than 1000\n");
-		cim1->Set(stick->GetY());
-		printf("Distance: %f4 \n",encoder->GetDistance());
-	} else {
-		cim1->Set(0);
-	}
-	*/
-	
+	/*
 	double distance = encoder->GetDistance();
 	
 	if (distance <=1000 && distance >=-1000 && stick->GetY()<=-0.05 || stick->GetY() >=0.05) {
@@ -57,7 +28,10 @@ void Winch::Move(Joystick* stick) {
 	else {
 		cim1->Set(0);
 	}
+	*/
+	//Put new code here
 	
+	//Code to bring in the winch when button 6 is presses
 	if (stick->GetRawButton(6) == true) {
 		BaneBot->Set(-0.35);
 	} else {
