f9052e4db010086d58bf5af6a3c9683836418c24
Arrowbots <team3705@gmail.com>
2013-03-25 19:30:40 -0300

    Update Winch.cpp
    Added the slow robot code, with all the ranges working properly, and also stopping at the set encoder distance.

--- Subsystems/Winch.cpp
+++ Subsystems/Winch.cpp
@@ -3,77 +3,87 @@
 #include "../Commands/MoveWinch.h"
 #include "SmartDashboard/SmartDashboard.h"
 
-static int setpoint=0;
-static int setpointGain=100;
-static double encoderValue=0;
-static double stickY=0;
-static double PropGain= 0.005;
-static int PropMax=100;
-static double cimSpeed=0;
-
-//Declare prototype so that we can put our function anywhere.
-int PropSpeed (int set, double encoder, double gain, int max);
-
-Winch::Winch() :
-  Subsystem("Winch") {
-  cim1 = RobotMap::driveTraincim1; //Test cim
-  cim2 = RobotMap::driveTraincim2; //Not sure why this is even here!!! JAVED!!
-  cim3 = RobotMap::winchcim3; //Winch cim
-  cim4 = RobotMap::winchcim4; //Winch cim
-  encoder = RobotMap::winchencoder;
-  baseLimit = RobotMap::relayLimitSwitch3; //Limit switch at the base of the winch
-  BaneBot = RobotMap::BaneBot;
-}
+double encoderOld = 0;
+double encoderMax = 4000;
+double encoderMin = -4000;
 
 void Winch::InitDefaultCommand() {
-  SetDefaultCommand(new MoveWinch());
+	SetDefaultCommand(new MoveWinch());
 }
+
+Winch::Winch() :
+			Subsystem("Winch") {
+
+	cim1 = RobotMap::driveTraincim1; //Test cim
+	cim3 = RobotMap::winchcim3; //Winch cim
+	cim4 = RobotMap::winchcim4; //Winch cim
+	encoder = RobotMap::winchencoder;
+	baseLimit = RobotMap::relayLimitSwitch3; //Limit switch at the base of the winch
+	BaneBot = RobotMap::BaneBot;
+
+}
+
 void Winch::Move(Joystick* stick) {
-  /*
-    double distance = encoder->GetDistance();
-    
-    if (distance <=1000 && distance >=-1000 && stick->GetY()<=-0.05 || stick->GetY() >=0.05) {
-    cim1->Set(stick->GetY());
-    }
-    else {
-    cim1->Set(0);
-    }
-  */
-  
-  //Daniel's Proportional Code :)
-  encoderValue=encoder->GetDistance(); //Store the encoder value 
-  if (stick->GetY() >= 0.05) {
-    setpoint += (stick->GetY())*setpointGain;
-  } else if (stick->GetY() <= -0.05) {
-    setpoint -= (stick->GetY())*setpointGain;
-  }
-  cimSpeed = PropSpeed(setpoint, encoderValue, PropGain, PropMax);
-  cim1->Set(cimSpeed);
 
-  //Code to bring in the winch when button 6 is pressed
-  if (stick->GetRawButton(6) == true) {
-    BaneBot->Set(-0.35);
-  } else {
-    BaneBot->Set(0.0);
-  }
-}
+	double distance = encoder->GetDistance(); // Get distance from encoder, and store it in a variable.
 
-int PropSpeed (int set, double encoder, double gain, int max) {
-  int difference = (set - encoder);
-  if (difference > max) {
-    difference= max;
-  } else if (difference < (max*(-1))) {
-    difference=max*(-1);
-  }
-  int out = difference*gain;
-  return out;
+	//Check to see if the distance is greater than encoder min, and make sure joystic value 
+	if ((distance >= encoderMin) && stick->GetY() >= 0.05) { 
+		printf("hi\n");
+		//If the distance is less than or equal to 2000 and distance is less than or equal to 4000.
+		if (distance >= (2000) && distance <= encoderMax) {
+			cim1->Set(0.7); //Set the cim speed to 10% counterclockwise
+			printf("hi slow\n");
+		} 
+		else if (distance >= encoderMax && distance <=10000){
+			cim1->Set(0);
+			printf("Turned off \n");
+		}
+		else {
+			cim1->Set(stick->GetY());
+		}
+
+		//If the distance is less than or equal to -2000 and distance is less than or equal to 4000.
+	} else if ((distance <= encoderMax) && stick->GetY() <= -0.05) {
+		printf("low\n");
+		if (distance <= (-2000 )&& distance >= encoderMin) {
+			printf("low slow\n");
+			cim1->Set((-0.7));
+		} 
+		else if (distance <= encoderMin && distance >=-10000){
+			cim1->Set(0);
+			printf("Turned off \n");
+		}
+		else {
+			cim1->Set(stick->GetY());
+		}
+
+	} else {
+		cim1->Set(0);
+	}
+
+	//Code to bring in the winch when button 6 is pressed
+	if (stick->GetRawButton(6) == true) {
+		BaneBot->Set(-0.35);
+	} else {
+		BaneBot->Set(0.0);
+	}
+
+	if (encoderOld != distance) {
+		printf("Encoder: %d \n", int(distance));
+		encoderOld = distance;
+	}
+
+	if (stick->GetRawButton(7) == true) {
+		encoder->Reset();
+	}
 }
 
 void Winch::Stop() {
 	cim3->Set(0);
 	cim4->Set(0);
 	BaneBot->Set(0);
-	//encoder->Stop(); //Stop the encoder
-	//bencoder->Reset(); //Reset the encoder
+	encoder->Stop(); //Stop the encoder
+	encoder->Reset(); //Reset the encoder
 }
 
