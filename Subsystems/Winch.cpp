2f3e4a8c565119dde376fb64c38c0d292b37b751
Arrowbots <team3705@gmail.com>
2013-03-26 16:24:54 -0300

    Added some comments

--- Subsystems/Winch.cpp
+++ Subsystems/Winch.cpp
@@ -27,37 +27,40 @@ void Winch::Move(Joystick* stick) {
 
 	double distance = encoder->GetDistance(); // Get distance from encoder, and store it in a variable.
 
-	//Check to see if the distance is greater than encoder min, and make sure joystic value 
+	//Check to see if the distance is greater than -4000 (which is represented by encoderMIn), and make sure joystic value 
 	if ((distance >= encoderMin) && stick->GetY() >= 0.05) { 
-		printf("hi\n");
+		printf("winch extending\n");
 		//If the distance is less than or equal to 2000 and distance is less than or equal to 4000.
 		if (distance >= (2000) && distance <= encoderMax) {
-			cim1->Set(0.7); //Set the cim speed to 10% counterclockwise
-			printf("hi slow\n");
-		} 
+			cim1->Set(0.7); //Set the cim speed to 70% counterclockwise
+			printf("slow down winch\n");
+		} //if the distance is more than encoderMax(4000) and distance is less than 10000
 		else if (distance >= encoderMax && distance <=10000){
-			cim1->Set(0);
-			printf("Turned off \n");
+			cim1->Set(0);//don't move the winch
+			printf("WINCH CANNOT MOVE\n");
 		}
 		else {
 			cim1->Set(stick->GetY());
 		}
 
-		//If the distance is less than or equal to -2000 and distance is less than or equal to 4000.
+		//If the distance is less than or equal to 4000 (which is represented by encoderMax).
 	} else if ((distance <= encoderMax) && stick->GetY() <= -0.05) {
-		printf("low\n");
+		printf("winch retracting\n");
+		//if the encoder distance is less than -2000 and the distance is more than encoderMin(-4000)
 		if (distance <= (-2000 )&& distance >= encoderMin) {
-			printf("low slow\n");
+			printf("slow down winch retraction\n");
 			cim1->Set((-0.7));
-		} 
+		}
+		//if the encoder distance is less than encoderMin(-4000) and encoder distance is more than -10000
 		else if (distance <= encoderMin && distance >=-10000){
 			cim1->Set(0);
-			printf("Turned off \n");
+			printf("WINCH CANNOT MOVE\n");
 		}
+		//else just operate normally
 		else {
 			cim1->Set(stick->GetY());
 		}
-
+	//else don't activate cim
 	} else {
 		cim1->Set(0);
 	}
@@ -69,11 +72,13 @@ void Winch::Move(Joystick* stick) {
 		BaneBot->Set(0.0);
 	}
 
+	//print back encoder distance
 	if (encoderOld != distance) {
 		printf("Encoder: %d \n", int(distance));
 		encoderOld = distance;
 	}
 
+	//reset the encoder
 	if (stick->GetRawButton(7) == true) {
 		encoder->Reset();
 	}
